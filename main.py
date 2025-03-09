0b1024ffa4e8310daf5a9234eb531c1b46a67be5
jensstockhausen <jens@famst.de>
2025-03-09 20:34:41 +0100

    add safe height parameter to cut function and update Cut class instantiation

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from fc4cnc.cut import Cut
 app = typer.Typer()
 
 @app.command()
-def cut(length: float, depth: float, step_down: float = 1.2, speed: float = 400):
+def cut(length: float, depth: float, 
+        step_down: float = 1.2, speed: float = 400, safe_height: float = 2):
     print(f"Creating a cut starting at X,Y home {length}mm long {depth}mm deep")
-    print(f"using step down {step_down}mm feed{speed}mm/min")
-    c = Cut(length, depth, step_down, speed)
+    print(f"using step down {step_down}mm feed{speed}mm/min safe height {safe_height}mm" )
+    c = Cut(length=length, depth=depth, step_down=step_down, speed=speed, safe_height=safe_height)
 
     gcode = c.gcode()
     print(gcode)
